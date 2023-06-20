6b460918bc7be6c3de1e6d3f1a043817218c5c58
lucio-crDEV <lucio.m.cardenas@gmail.com>
2023-06-20 11:23:40 -0400

    devolviendo la ruta /home

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from datos_proyectos import proyectos, buscar_proyecto_por_id
 from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return redirect('/home')
+
+@app.route('/home')
 def portafolio():
     return render_template('portafolio.html', proyectos=proyectos)
 
