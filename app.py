54407bc69515039ec34e2772cdc1015ab1dfc008
lucio-crDEV <lucio.m.cardenas@gmail.com>
2023-07-04 08:37:21 -0400

    agregando manejo de error de url

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from datos_proyectos import proyectos
-from flask import request
 
 app = Flask(__name__)
 
@@ -11,6 +10,10 @@ def index():
 @app.route('/home')
 def portafolio():
     return render_template('portafolio.html', proyectos=proyectos)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
